b40253c2930abbba84776fae663d2fccd20372b8
charity-bit <charitynyanchera@gmail.com>
2022-05-01 10:13:03 +0300

    ft:get-sources -created the function that fetches the sources

--- app/requests.py
+++ app/requests.py
@@ -1,3 +1,4 @@
+from urllib import response
 from app import app
 import urllib.request
 import json
@@ -5,3 +6,47 @@ from .models import article,source
 
 Article = article.Article
 Source = source.Source
+
+BASE_SOURCE_URL = app.config['SOURCE_URL']
+BASE_ARTICLES_URL = app.config['ARTICLES_URL']
+API_KEY = app.config['NEWS_API_KEY']
+
+def get_sources():
+    source_url = BASE_SOURCE_URL.format(API_KEY)
+    with urllib.request.urlopen(source_url) as source:
+        data = source.read()
+        response = json.loads(data)
+        print(response)
+
+        results = None
+
+        if response['sources']:
+            sources_list = response['sources']
+            results = process_response(sources_list)
+
+        return results 
+
+
+
+    
+def process_response(response):
+    list = []
+   
+    for source in response:
+        if source['url']:
+                # (self,id,name,description,url,category,country):
+            source_instance = Source(source['id'],source['name'],source['description'],source['url'],source['category'],source['country'])
+            list.append(source_instance)
+    return list
+
+  
+
+
+
+
+def get_source_articles(source_id):
+    pass
+
+
+def get_articles():
+    pass
--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
+import imp
 from flask import render_template
 from app import app
+from .requests import get_sources
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    sources = get_sources()
+    return render_template('index.html',sources = sources)
