3f635b03b417822b48353e9052d1040fae58415c
charity-bit <charitynyanchera@gmail.com>
2022-05-01 17:20:34 +0300

    ft:routing -added routessports,entertainment,business and technology

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 class Config:
     ARTICLES_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
     # https://newsapi.org/v2/top-headlines?country=us&category=sports
-    GENERAL_URL = 'https://newsapi.org/v2/top-headlines?category={}&apiKey={}'
+    GENERAL_URL = 'https://newsapi.org/v2/top-headlines?language=en&category={}&apiKey={}'
     SOURCE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
   
 
--- app/views.py
+++ app/views.py
@@ -20,6 +20,27 @@ def sports():
     articles = get_articles('sports')
     return render_template('sports.html',articles = articles)
 
-@app.route('/politics')
+@app.route('/business')
+def politics():
+    articles = get_articles('business')
+    return render_template('business.html',articles = articles)
+
+@app.route('/technology')
+def tech():
+    articles = get_articles('technology')
+    return render_template('technology.html',articles = articles)
+
+@app.route('/entertainment')
+def entertainment():
+    articles = get_articles('entertainment')
+    return render_template('entertainment.html',articles = articles)
+
+@app.route('/health')
+def entertainment():
+    articles = get_articles('entertainment')
+    return render_template('entertainment.html',articles = articles)
+
+
+
 
 
