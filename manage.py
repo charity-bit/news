a561af8750ad0b43fd64745f9b0440300affcf49
charity-bit <charitynyanchera@gmail.com>
2022-05-02 14:27:14 +0300

    deployement to heroku

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from app import create_app
-from flask_frozen import Freezer
+
 from flask_script import Manager,Server
 
 app = create_app('production')
 manager = Manager(app)
-freezer = Freezer(app)
+
 manager.add_command('server',Server)
 
 @manager.command
