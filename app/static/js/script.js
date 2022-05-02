70bcf93012651593ead42bea3312b7e283a5b34b
charity-bit <charitynyanchera@gmail.com>
2022-05-02 14:43:56 +0300

    Clear git cache

--- app/static/js/script.js
+++
@@ -1,7 +0,0 @@
-const iconify = document.querySelector('.iconify')
-const navLinks = document.querySelector('#navlinks')
-
-iconify.addEventListener('click',()=>{
-    
-    navLinks.classList.toggle('show-links')
-})
