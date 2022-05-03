bc89029c50b4dc0bf34ed161d65b338d0fa7e20f
charity-bit <charitynyanchera@gmail.com>
2022-05-03 19:14:00 +0300

    created title headers for each page

--- app/main/views.py
+++ app/main/views.py
@@ -13,37 +13,37 @@ def index():
 
 @main.route('/articles/<source_id>')
 def articles(source_id):
-    title = 'articles'
+    title = 'Articles'
     articles = get_source_articles(source_id)
     return render_template('articles.html',articles = articles,title = title)
 
 @main.route('/sports')
 def sports():
-    title = 'sports'
+    title = 'Sports'
     articles = get_articles('sports')
     return render_template('sports.html',articles = articles,title = title)
 
 @main.route('/business')
 def politics():
-    title ='business'
+    title ='Business'
     articles = get_articles('business')
     return render_template('business.html',articles = articles,title = title)
 
 @main.route('/technology')
 def tech():
-    title = 'technology'
+    title = 'Technology'
     articles = get_articles('technology')
     return render_template('technology.html',articles = articles,title = title)
 
 @main.route('/entertainment')
 def entertainment():
-    title = 'entertainment'
+    title = 'Entertainment'
     articles = get_articles('entertainment')
     return render_template('entertainment.html',articles = articles,title = title)
 
 @main.route('/health')
 def health():
-    title = 'health'
+    title = 'Health'
     articles = get_articles('health')
     return render_template('health.html',articles = articles,title = title)
 
